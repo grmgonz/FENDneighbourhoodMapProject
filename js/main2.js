341e14bd6b14b29e218d3c9f9b1abe421f545e08
Gabe Gonzales <grmgonz@gmail.com>
2016-08-22 16:14:21 -0600

    2nd Commit

--- js/main2.js
+++ js/main2.js
@@ -2,6 +2,8 @@ var map;
 var marker = [];
 var infowindow;
 
+
+//MODEL
 //List of schools in Toronto
 var schools = [
 
@@ -43,99 +45,9 @@ var schools = [
     }
 
 ];
-//VIEW MODEL for utilizing Knockout: http://knockoutjs.com/
 
-var ViewModel = {
-
-    allSchools: ko.observableArray(schools),
-
-    //Displaying infowindow containing content from Wikipedia
-    displayWikiInfo: function(marker) {
-
-        infowindow.open(map, marker);
-        toggleBounce(marker);
-
-        //Setting the school variable in order to form the Wikipedia Url for each school
-        var selectSchool;
-        for (var i = 0; i < schools.length; i++) {
-            //Position of schools are specified with precise lat & lng coordinates fixed to the function
-            if (marker.position.lat().toPrecision(8) == schools[i].latitude.toPrecision(8)
-                && marker.position.lng().toPrecision(8) == schools[i].longitude.toPrecision(8)) {
-                selectSchool = schools[i].name;
-            }
-
-}
-    //Append schools to Wikipedia url
-        var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + selectSchool +
-            '&format=json';
-
-
-
-        $.ajax({
-            url: wikiUrl,
-            dataType: "jsonp"
-        }).done(function(data) {
-            var info = $(".info");
-            info.html("");
-            //Set error handling, if Wikipedia doesn't find any info.
-            if (data[1].length === 0) {
-                info.html("Sorry!!! There is no information related to this school");
-            } else {
-                var infowindowContent = '<ul>';
-                for (var i = 0; i < 1; i++) {
-
-                    var website = "View School's Website Here";
-
-                    if (data[1][i]) {
-                        infowindowContent += '<a href="' + data[3][i] + '" target="_blank"><h2>' + data[1][i]
-                        + '</h2></a><p>' + data[2][i] + '</p>' + '<p class="via">Info via Wikipedia</p>' + '<h4>' + '<a href=" '
-                        + marker.content + '" target="_blank">' + website + '</a>' + '</h4>';
-                    }
-                }
-                infowindowContent += '</ul>';
-                info.html(infowindowContent);
-            }
-        }).fail(function() {
-            $(".info").html("Fail to load wiki content");
-        });
-
-    },
-
-
-    //Function for when schools are clicked in the sidebar menu
-    schoolClicked: function(school) {
-
-
-        var schoolList;
-        for (var i = 0; i < schools.length; i++) {
-            if (schools[i].name === school.name) {
-                schoolList = i;
-            }
-        }
-        ViewModel.displayWikiInfo(marker[schoolList]);
-
-
-    },
-    //now let's handle the search filter function
-    query: ko.observable(''),
-    search: function(value) {
-        // remove all the current locations, which removes them from the view
-        ViewModel.allSchools([]);
-        // remove all marker now, than add them back
-        for (var ii = 0; ii < marker.length; ii++) {
-            marker[ii].setMap(null);
-        }
-        console.log(value);
-        for (var x in schools) {
-            if (schools[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-                ViewModel.allSchools.push(schools[x]);
-                marker[x].setMap(map);
-            }
-        }
-    }
-};
-
-// The function to initialize the Google Map
+//VIEW
+// Function to initialize the Google Map
 function initMap() {
 
     //Adding my own style to the map
@@ -151,7 +63,7 @@ function initMap() {
 
 //Set infowinow content
     infowindow = new google.maps.InfoWindow({
-        content: "<div style = 'width:400px;min-height:150px'>" + '<div class="info"></div>' + '<div id ="URLlink"></div>'
+        content: "<div style = 'width:400px;min-height:150px'>" + '<div class="info"></div>'
     });
 //Load Map
     map = new google.maps.Map(document.getElementById('map'), {
@@ -175,8 +87,6 @@ function initMap() {
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 64)
     };
-
-     var bounds = new google.maps.LatLngBounds();
 
 
      //Loop through KO Observable Array to display markers
@@ -210,11 +120,19 @@ function initMap() {
             this.setIcon(defaultIcon);
         });
 
-  bounds.extend(LatLng);
+
 
     }
-    map.fitBounds(bounds);
+
+function googleError() {
+    if (typeof google === "undefined") {
+        $('#map').html('<h1>' + "Fail to load Google maps.  Please refresh and try again later." + '</h1>');
+    }
 }
+
+
+}
+
 
 //Function for marker animation
 function toggleBounce(marker) {
@@ -230,9 +148,101 @@ function toggleBounce(marker) {
 
 
 
+//VIEW MODEL for utilizing Knockout: http://knockoutjs.com/
+
+var ViewModel = {
+
+    allSchools: ko.observableArray(schools),
+
+//Displaying infowindow containing content from Wikipedia
+    displayWikiInfo: function(marker) {
+    infowindow.open(map, marker);
+    toggleBounce(marker);
+
+//Setting the school variable in order to form the Wikipedia Url for each school
+    var selectSchool;
+    for (var i = 0; i < schools.length; i++) {
+//Position of schools are specified with precise lat & lng coordinates fixed to the function
+        if (marker.position.lat().toPrecision(8) == schools[i].latitude.toPrecision(8)
+            && marker.position.lng().toPrecision(8) == schools[i].longitude.toPrecision(8)) {
+            selectSchool = schools[i].name;
+            }
+
+}
+//Append schools to Wikipedia url
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + selectSchool +
+            '&format=json';
+
+
+//Async AJAX request
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp"
+        }).done(function(data) {
+            var info = $(".info");
+            info.html("");
+            //Set error handling, if Wikipedia doesn't find any info.
+            if (data[1].length === 0) {
+                info.html('<h5>' + "Sorry!!! There is no information related to this school" + '</h5>');
+            } else {
+                var infowindowContent = '<ul>';
+                for (var i = 0; i < 1; i++) {
+
+                    var website = "View School's Website Here";
+
+                    if (data[1][i]) {
+                        infowindowContent += '<a href="' + data[3][i] + '" target="_blank"><h2>' + data[1][i]
+                        + '</h2></a><p>' + data[2][i] + '</p>' + '<p class="via">Info via Wikipedia</p>' + '<h4>' + '<a href=" '
+                        + marker.content + '" target="_blank">' + website + '</a>' + '</h4>';
+                    }
+                }
+                infowindowContent += '</ul>';
+                info.html(infowindowContent);
+            }
+        }).fail(function() {
+            $(".info").html('<h5>' + "OOPS, content from Wikipedia has failed to load" + '</h5>');
+        });
+
+    },
+
+
+    //Function for when schools are clicked in the sidebar menu
+    schoolClicked: function(school) {
+
+
+        var schoolList;
+        for (var i = 0; i < schools.length; i++) {
+            if (schools[i].name === school.name) {
+                schoolList = i;
+            }
+        }
+        ViewModel.displayWikiInfo(marker[schoolList]);
+
+
+    },
+    //Here's the search function
+    filtering: ko.observable(''),
+    search: function(value) {
+        //Remove the current locations from the view
+        ViewModel.allSchools([]);
+        //Remove markers and add them back depending on what is being searched
+        for (var s = 0; s < marker.length; s++) {
+            marker[s].setMap(null);
+        }
+
+        for (var t in schools) {
+            if (schools[t].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                ViewModel.allSchools.push(schools[t]);
+                marker[t].setMap(map);
+            }
+        }
+    }
+};
 
 
 
-// Activates knockout.js
+
+
+//Initiate Knockout (The View Model part of the project)
 ko.applyBindings(ViewModel);
-ViewModel.query.subscribe(ViewModel.search);
+ViewModel.filtering.subscribe(ViewModel.search);
